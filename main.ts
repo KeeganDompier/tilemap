57adad147b5b34433f24b9f4d03f1ca5939b5169
KeeganDompier <58610698+KeeganDompier@users.noreply.github.com>
2020-03-02 13:03:03 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,115 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+}
+function buildLevel () {
+    // If level equals length of array length, then YOU
+    // WIN
+    //
+    // Array length is 2: 0, 1, 2
+    if (nextLevel == levels.length) {
+        game.over(true)
+    }
+    scene.setTileMap(levels[nextLevel])
+    // places the player at a certain position when
+    // touching orange tile
+    for (let value of scene.getTilesByType(4)) {
+        value.place(player1)
+    }
+    // the array of maps
+    nextLevel += 1
+}
+scene.onHitTile(SpriteKind.Player, 7, function (sprite) {
+    buildLevel()
+})
+let player1: Sprite = null
+let nextLevel = 0
+let levels: Image[] = []
+// sorting levels in an array list
+levels = [img`
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f 4 f f f f f f 7 f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+f f f f f f f f f f 
+`, img`
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+9 4 9 9 9 9 9 9 7 9 
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+9 9 9 9 9 9 9 9 9 9 
+`, img`
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+3 4 3 3 3 3 3 3 7 3 
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+3 3 3 3 3 3 3 3 3 3 
+`]
+// It makes the block look like a face and creates a
+// wall
+scene.setTile(7, img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 f 7 7 7 7 f 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 f 7 7 7 7 7 7 7 7 f f 7 7 7 
+7 7 7 f f 7 7 7 7 7 7 f 7 7 7 7 
+7 7 7 7 f f 7 7 7 7 f f 7 7 7 7 
+7 7 7 7 7 f f f f f f 7 7 7 7 7 
+7 7 7 7 7 7 7 f f 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+`, true)
+// It starts a number for next level
+nextLevel = 0
+player1 = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 2 2 2 2 2 . . . . . . . . 
+. . 2 2 . . . 2 2 . . . . . . . 
+. . 2 . 7 . 7 . 2 . . . . . . . 
+. . 2 2 . . . 2 2 . . . . . . . 
+. . . 2 2 2 2 2 . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . 2 2 2 2 2 . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+. . . . 2 2 2 . . . . . . . . . 
+. . . 2 2 . 2 2 . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, SpriteKind.Player)
+controller.moveSprite(player1, 100, 100)
+buildLevel()
